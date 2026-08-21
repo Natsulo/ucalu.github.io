f7a33b15f6dfee2cba1d486420f3fd7748241f1d
joaquim <Amogussus3012@gmail.com>
2026-08-21 15:20:33 -0300

    arrumei o botao

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const botao = document.getElementById("butaowo")
+const botao = document.button
 const body = document.body
 const h1 = document.querySelector("h1")
 botao.addEventListener("click", function () {
     body.style.backgroundColor = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
     h1.style.color = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
-    button.style.backgroundColor = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
+    botao.style.backgroundColor = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
 })
