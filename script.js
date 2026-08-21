0df8fe9045acfc46badb2efd700535d9387e2c95
joaquim <Amogussus3012@gmail.com>
2026-08-21 15:22:36 -0300

    xicletinho

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const botao = document.button
+const botao = document.getElementbyId("botaowo")
 const body = document.body
 const h1 = document.querySelector("h1")
-botao.addEventListener("click", function () {
+botao.addEventListener("click", function() {
     body.style.backgroundColor = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
     h1.style.color = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
     botao.style.backgroundColor = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
