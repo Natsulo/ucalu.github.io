23373fd83c09e8476085bfddde0ddd6d2f7cebec
joaquim <Amogussus3012@gmail.com>
2026-08-21 15:57:03 -0300

    tentando de novo

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ botao.addEventListener("click", function() {
     h1.style.color = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
     botao.style.backgroundColor = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
     botao.style.borderColor = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
-    h1.style.position = `bottom: ${Math.floor(Math.random() * 151)} px`
-    h1.style.position = `left: ${Math.floor(Math.random() * 151)} px`
-    h1.style.position = `right: ${Math.floor(Math.random() * 151)} px`
-    h1.style.position = `top: ${Math.floor(Math.random() * 151)} px`
+    h1.style.position = 'absolute'
+    h1.style.position = `left: ${Math.floor(Math.random() * 151)}px`
+    h1.style.position = `top: ${Math.floor(Math.random() * 151)}px`
 })
