5a624ed4bb2b5d0436b7ab1881df7cd627913a9a
joaquim <Amogussus3012@gmail.com>
2026-08-21 15:26:39 -0300

    botão de novo

--- script.js
+++
@@ -1,8 +0,0 @@
-const botao = document.getElementbyId("botaowo")
-const body = document.body
-const h1 = document.querySelector("h1")
-botao.addEventListener("click", function() {
-    body.style.backgroundColor = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
-    h1.style.color = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
-    botao.style.backgroundColor = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
-})
