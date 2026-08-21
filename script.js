25a3130268d8f1c4f2cd9ee5e5c953d824b21f49
joaquim <Amogussus3012@gmail.com>
2026-08-21 15:14:54 -0300

    mais randomizadores

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const botao = document.getElementById("botaowo")
+const botao = document.button
 const body = document.body
+const h1 = document.h1
 botao.addEventListener("click", function () {
     body.style.backgroundColor = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
+    h1.style.Color = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
+    button.style.backgroundColor = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
 })
