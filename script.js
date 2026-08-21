68d620e3f2e4109e99725e647a50dbd378ecc0a5
joaquim <Amogussus3012@gmail.com>
2026-08-21 15:18:59 -0300

    arrumei o randomizador

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const botao = document.getElementById("butaowo")
 const body = document.body
-const h1 = document.h1
+const h1 = document.querySelector("h1")
 botao.addEventListener("click", function () {
     body.style.backgroundColor = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
-    h1.style.Color = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
+    h1.style.color = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
     button.style.backgroundColor = `rgb(${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))})`
 })
